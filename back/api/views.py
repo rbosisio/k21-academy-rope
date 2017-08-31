668711519b9206695bf7e4f308df421b197473ac
rodrigobosisio <bosisio@k21.com.br>
2017-08-31 14:53:05 -0300

    Criando usuario para o django-admin

--- back/api/views.py
+++ back/api/views.py
@@ -9,4 +9,7 @@ class ApiDreams(APIView):
 	renderer_classes = (JSONRenderer, )
 
 	def get(self,request):
-		return Response({'msg': 'works'})
+		return Response({'msg': 'todos os sonhos'})
+
+	def post(self,request):
+		return Response({'msg': 'novo sonho'})
