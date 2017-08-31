4f9ff99df1d84a2e1098e84d1298bd36f567288a
Alex Albuquerque <alex@bazzah.com.br>
2017-08-31 15:29:25 -0300

    criando modelo Dream

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+
+class DreamAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Dream, DreamAdmin)
--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,31 @@
+# encoding: utf-8
+
 from django.db import models
 
 # Create your models here.
+
+class Dream(models.Model):
+
+	STATUS = (
+        ('novo', 'Novo'),
+        ('inviavel', 'Inviavel'),
+        ('pre_aprovado', 'Pré-Aprovado'),
+    )
+
+	name = models.CharField('Nome', max_length=255, default=None)
+	age = models.CharField('Idade', max_length=255, default=None)
+	email = models.CharField('E-mail', max_length=255, default=None)
+	phone = models.CharField('Telefone', max_length=255, default=None)
+	inmate = models.BooleanField('Internado', default=False)
+	hospital_name = models.CharField('Nome do Hospital', max_length=255, default=None)
+	hospital_contact = models.CharField('Contato do Hospital', max_length=255, default=None)  
+	medical_approved = models.BooleanField('Aprovação Médica', default=False)
+	parental_approved = models.CharField('Aprovação do Responsável', max_length=255, default=None)
+	description = models.TextField('Descrição', default=None)
+	hospital_address = models.CharField('Endereço do Hospital', max_length=255, default=None)
+	observation = models.CharField('Observação', max_length=255, default=None)
+	liason = models.TextField('Relacionamento com Paciente', default=None)
+	status = models.CharField('Status', max_length=1, choices=STATUS)
+	planning_description = models.TextField('Descrição do Planejamento', default=None)
+
+
