fc3396035dabbc7bffd941c437fa7ac70f88fa36
allan <allan@k21.com.br>
2017-09-01 10:08:50 -0300

    Colocando default NOVO no campo status do modelo de sonho

--- back/api/models.py
+++ back/api/models.py
@@ -29,7 +29,7 @@ class Dream(models.Model):
 	hospital_address = models.CharField('Endereço (onde paciente está)', max_length=255, default=None, null=True)
 	observation = models.TextField('Observação', default=None)
 	liason = models.CharField('Relacionamento com Paciente', max_length=255, default=None)
-	status = models.CharField('Status', max_length=20, choices=STATUS)
+	status = models.CharField('Status', max_length=20, choices=STATUS, default='novo')
 	planning_description = models.TextField('Descrição do Planejamento', default=None)
 
 	class Meta:
